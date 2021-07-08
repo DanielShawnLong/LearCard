0c77bcf6bfc22258424cff464c886de812d57487
Daniel-Shawn Long <danielshawnlong@protonmail.com>
2021-07-08 18:01:59 +0200

    Initialize commit

---
+++ src/main/java/htwsaar/ariadne/lernkartei/LernkarteiApplication.java
@@ -0,0 +1,13 @@
+package htwsaar.ariadne.lernkartei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LernkarteiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LernkarteiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/htwsaar/ariadne/lernkartei/LernkarteiApplicationTests.java
@@ -0,0 +1,13 @@
+package htwsaar.ariadne.lernkartei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LernkarteiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
