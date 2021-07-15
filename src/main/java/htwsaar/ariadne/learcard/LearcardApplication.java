33401e68b003488f92b3e9f9036803279afb768c
Daniel-Shawn Long <danielshawnlong@protonmail.com>
2021-07-15 18:32:43 +0200

    Initial commit

---
+++ src/main/java/htwsaar/ariadne/learcard/LearcardApplication.java
@@ -0,0 +1,13 @@
+package htwsaar.ariadne.learcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearcardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearcardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/htwsaar/ariadne/learcard/LearcardApplicationTests.java
@@ -0,0 +1,13 @@
+package htwsaar.ariadne.learcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearcardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
