289a93feb7fb8ed3c8c5bac778f42466439c65d4
Felix <felix.werner92@gmail.com>
2021-10-10 15:24:34 +0200

    service impl + Kommentare

--- api/src/main/java/htwsaar/ariadne/learcard/security/service/interfaces/LearnCardGroupService.java
+++ api/src/main/java/htwsaar/ariadne/learcard/security/service/interfaces/LearnCardGroupService.java
@@ -7,13 +7,41 @@ import java.util.Optional;
 
 public interface LearnCardGroupService {
 
+    /**
+     * Sucht eine Learncardgroup anhand ihrer id und Namens.
+     * @param id
+     * @param name
+     * @return Learncardgroup
+     */
+
     LearnCardGroup findByIdAndUserName(long id, String name);
 
+    /**
+     *  Entfernt eine LearnCardGroup aus der Datenbank.
+     * @param id
+     * @param name
+     */
     void deleteByIdAndUserName(long id, String name);
 
+    /**
+     * Gibt eine Liste aller Learncardgroups eines Nutzers aus.
+     * @param username
+     * @return List Learncardgroup
+     */
     List<LearnCardGroup> findByUserName(String username);
 
+    /**
+     * Sucht eine Learncardgroup anhand ihrer ID.
+     * @param id
+     * @return
+     */
     Optional<LearnCardGroup> findById(long id);
+
+    /**
+     * Legt eine Learncardgroup in der Datenbank an.
+     * @param group
+     * @return Learncardgroup
+     */
 
     LearnCardGroup save(LearnCardGroup group);
 
--- api/src/main/java/htwsaar/ariadne/learcard/security/service/interfaces/LearnCardService.java
+++ api/src/main/java/htwsaar/ariadne/learcard/security/service/interfaces/LearnCardService.java
@@ -8,24 +8,78 @@ import java.util.Optional;
 
 public interface LearnCardService {
 
+    /**
+     * Gibt eine Liste aller Lernkarten zurück.
+     * @return List Learncard
+     */
     List<LearnCard> getAll();
 
+    /**
+     * Trägt eine Learncard in die Datenbank ein und gibt sie zurück.
+     * @param card
+     * @return Learncard
+     */
     LearnCard insertCard(LearnCard card);
+
+    /**
+     * Sucht eine Learncard anhand ihres namens und ihrer Id
+     * @param id
+     * @param name
+     * @return Learncard
+     */
 
     LearnCard findCardByIdandName(long id, String name);
 
+    /**
+     * Sucht eine Learncard anhand ihrer id
+     * @param id
+     * @return Learncard
+     */
+
     Optional<LearnCard> findCardById(long id);
 
+    /**
+     * Sucht alle Learncards eines Users.
+     * @param name
+     * @return List Learncard
+     */
     List<LearnCard> findAllByName(String name);
 
+    /**
+     * Entfernt eine Learncard aus der Datenbank.
+     * @param id
+     * @param name
+     */
     void removeCard(long id,String name);
 
+    /**
+     * Erstellt eine Liste aller Learncards eines Nutzer und einer LearncardGroup
+     * @param group
+     * @param username
+     * @return List Learncard
+     */
     List<LearnCard> findByGroupIdAndUserName(Long group, String username);
 
+
+    /**
+     * Gibt eine Liste aller Learncards eines Nutzers und einer Gruppe, welche richtig beantwortet wurden.
+     * @param group
+     * @param username
+     * @param answer
+     * @return List Learncard
+     */
     List<LearnCard> findByGroupIdAndUserNameAndRightAnswer(Long group,
                                                            String username,
                                                            Boolean answer);
 
+    /**
+     * Gibt eine Liste aller Learncards eines Nutzers und einer Gruppe, welche richtig beantwortet wurden.
+     * @param group
+     * @param username
+     * @param rightAnswer
+     * @param isSolved
+     * @return List Learncard
+     */
     List<LearnCard> findByGroupIdAndUserNameAndRightAnswerAndIsSolved(Long group,
                                                                       String username,
                                                                       Boolean rightAnswer,
